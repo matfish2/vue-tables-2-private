13c107673cd7e2c12f499a5210f910dbf2eea540
Matanya Fishaimer <matfish2@gmail.com>
2020-09-25 13:50:41 +0300

    add vt row selector template

---
+++ templates/VtRowSelector.vue
@@ -0,0 +1,22 @@
+<template>
+    <td :class="`VueTables__select-row VueTables__select-single ${props.tdClass}`"
+        @click="props.toggleRow($event, props.row, props.index, props.disabled)">
+        <input :type="props.type"
+               name="selected_row[]"
+               class="vt-select-row"
+               :disabled="props.disabled"
+               :checked="props.selected"
+        />
+    </td>
+</template>
+
+<script>
+    export default {
+        name: "MyVtRowSelector",
+        props: ['props']
+    }
+</script>
+
+<style scoped>
+
+</style>
