46ee1250f5ef7c1106b839f19cccc94641c06644
Matanya Fishaimer <matfish2@gmail.com>
2020-11-16 21:14:03 +0200

    fix filters

--- compiled/methods/filter-type.js
+++ compiled/methods/filter-type.js
@@ -1,8 +1,16 @@
 "use strict";
 
+var _VtTextFilter = _interopRequireDefault(require("../components/VtTextFilter"));
+
+var _VtListFilter = _interopRequireDefault(require("../components/VtListFilter"));
+
+var _VtDateFilter = _interopRequireDefault(require("../components/VtDateFilter"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
 module.exports = function (column) {
   if (!this.opts.filterable) return false;
-  if (this.isTextFilter(column)) return 'vt-text-filter';
-  if (this.isDateFilter(column)) return 'vt-date-filter';
-  if (this.isListFilter(column)) return 'vt-list-filter';
+  if (this.isTextFilter(column)) return _VtTextFilter["default"];
+  if (this.isDateFilter(column)) return _VtDateFilter["default"];
+  if (this.isListFilter(column)) return _VtListFilter["default"];
 };
