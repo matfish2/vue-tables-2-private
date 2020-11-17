d925be8e6b196744742a33b59a56f262ff3e6af5
Matanya Fishaimer <matfish2@gmail.com>
2020-11-17 18:19:02 +0200

    fix

--- compiled/helpers/emitted-events.js
+++ compiled/helpers/emitted-events.js
@@ -4,5 +4,5 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports["default"] = void 0;
-var _default = ['loading', 'loaded', 'intersect', 'pagination', 'filter', 'sorted'];
+var _default = ['loading', 'loaded', 'intersect', 'pagination', 'filter', 'sorted', 'select'];
 exports["default"] = _default;
