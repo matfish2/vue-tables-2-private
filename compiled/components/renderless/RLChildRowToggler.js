6f9ff37ced5243062c9bbe34bfadcbc30ec8bd40
Matanya Fishaimer <matfish2@gmail.com>
2021-05-28 10:47:56 +0300

    add disabled child row togglers

--- compiled/components/renderless/RLChildRowToggler.js
+++ compiled/components/renderless/RLChildRowToggler.js
@@ -7,16 +7,27 @@ exports["default"] = void 0;
 var _default = {
   name: 'RLChildRowToggler',
   props: ['rowId'],
-  inject: ['toggleChildRow', 'opts', 'theme', 'childRowTogglerClass', 'componentsOverride', 'tabIndex'],
+  inject: ['toggleChildRow', 'opts', 'theme', 'childRowTogglerClass', 'componentsOverride', 'tabIndex', 'row'],
   render: function render(h) {
+    var toggleable = this.isToggleable(this.opts().disabledChildRows);
     return this.$scopedSlots["default"]({
       opts: this.opts(),
       "class": this.childRowTogglerClass.bind(this, this.rowId),
-      toggle: this.toggleChildRow.bind(this, this.rowId),
+      toggle: toggleable ? this.toggleChildRow.bind(this, this.rowId) : function () {},
       override: this.componentsOverride.childRowToggler,
       tabIndex: this.tabIndex(),
-      theme: this.theme
+      theme: this.theme,
+      toggleable: toggleable
     });
+  },
+  methods: {
+    isToggleable: function isToggleable(callback) {
+      if (!callback) {
+        return true;
+      }
+
+      return !callback(this.row());
+    }
   }
 };
 exports["default"] = _default;
